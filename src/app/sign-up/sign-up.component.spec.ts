fb8207757f38574b1db65259b8c189f7dc98c47f
TheoCaesar <theocaesar7@gmail.com>
2024-09-10 16:08:13 +0100

    layout test suite

--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -21,8 +21,56 @@ describe('SignUpComponent', () => {
 
   it('should create', () => {
     let template = fixture.nativeElement as HTMLElement
-    let pElement:HTMLParagraphElement | null = template.querySelector("p")
-    expect(pElement?.innerHTML).toContain("sign-up works!");
-    expect(pElement?.innerHTML).toContain("sign-up works!");
+    let pElement:HTMLParagraphElement | null = template.querySelector("h1")
+    expect(pElement?.innerHTML).toContain("Sign Up");
   });
+});
+
+describe("Test Signup Component's Layout", ()=>{
+  let fixture: ComponentFixture<SignUpComponent>
+  let component: SignUpComponent;
+  let template: HTMLElement
+  
+  beforeEach(async ()=>{
+    await TestBed.configureTestingModule({
+      declarations:[SignUpComponent]
+    })
+    .compileComponents();
+  })
+
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(SignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges;
+    template = fixture.nativeElement as HTMLElement;
+  })
+
+  it('contains a form', ()=>{
+    let formEl = template.querySelector('form');
+    expect(formEl).toBeTruthy();
+  })
+
+  it ('contains inputs for user registration', ()=>{
+    let inputs = template.querySelectorAll('input');
+    let labels = template.querySelectorAll('label');
+    expect(inputs.length).toEqual(labels.length);
+    let unameLbl = template.querySelector('label[for="username"]') as HTMLLabelElement
+    let emailLbl = template.querySelector('label[for="email"]') as HTMLLabelElement
+    let pwdLbl = template.querySelector('label[for="password"]') as HTMLLabelElement
+    let pwdLbl2 = template.querySelector('label[for="confirm"]') as HTMLLabelElement
+    expect([unameLbl, emailLbl]).toBeTruthy();
+    expect([pwdLbl, pwdLbl2]).toBeTruthy();
+    let unameField = template.querySelector('input[id="username"]') as HTMLInputElement
+    let emailField = template.querySelector('input[type="email"]') as HTMLInputElement
+    let pwdField = template.querySelector('input[type="password"]') as HTMLInputElement
+    let pwdField2 = template.querySelector('input[id="confirm"]') as HTMLInputElement
+    expect([unameField,emailField]).toBeTruthy();
+    expect([pwdField, pwdField2]).toBeTruthy();
+  })
+
+  it('contains sign up button', ()=>{
+    let btn = template.querySelector('button[type="submit"]') as HTMLButtonElement;
+    expect(btn?.innerHTML).toContain('Sign Up')
+    expect(btn?.disabled).toBeTruthy();
+  })
 });
